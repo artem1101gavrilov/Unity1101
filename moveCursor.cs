045c01aee3bf4b95de0101fcc812ceb1efd7d052
artem1101gavrilov <artem1101gavrilov@gmail.com>
2019-10-18 21:16:36 +0300

    Add files via upload

---
+++ moveCursor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class moveCursor : MonoBehaviour
+{
+    private void Start()
+    {
+        InvokeRepeating("Move", 0.1f, 0.1f);
+    }
+
+    void Move () {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 tpos = transform.position;
+
+        Vector2 pos2 = pos - tpos;
+        if(pos2.magnitude > 1)
+            transform.position = tpos + pos2.normalized;
+        else
+            transform.position = tpos + pos2;
+    }
+}
